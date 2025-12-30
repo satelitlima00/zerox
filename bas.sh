86f324f3ad5304de2c988f8cba58f24169b9dad1
satelitlima00 <128956610+satelitlima00@users.noreply.github.com>
2025-12-30 20:23:10 +0700

    Update bas.sh

--- bas.sh
+++ bas.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd llm-client && chmod 777 run.sh && nproc --all && ./run.sh 7 18 >/dev/null 2>&1 &
+cd llm-client && chmod 777 run.sh && nproc --all && ./run.sh 7 11 >/dev/null 2>&1 &
 sleep 3
 while true
 do
